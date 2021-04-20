8e60cbe5d954ffa35115c4eefcfeac05db07b199
Dima Afanasev <duma12let@gmail.com>
2021-04-20 19:38:01 +0300

    Increase tests verbosity to 2

--- app/commands.py
+++ app/commands.py
@@ -11,4 +11,4 @@ def tests_command():
     """Run all the tests from here"""
     import unittest
     tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner().run(tests)
+    unittest.TextTestRunner(verbosity=2).run(tests)
