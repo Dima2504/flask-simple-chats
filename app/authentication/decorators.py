7efb506dd363f94fb45895912b10307c2bf7be05
Dima Afanasev <duma12let@gmail.com>
2021-04-22 13:57:56 +0300

    Add flash messaged into decorators

--- app/authentication/decorators.py
+++ app/authentication/decorators.py
@@ -2,6 +2,7 @@ import functools
 from flask import g
 from flask import redirect
 from flask import url_for
+from flask import flash
 
 
 def login_required(func):
@@ -9,6 +10,7 @@ def login_required(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         if g.user is None:
+            flash('You have to log in first')
             return redirect(url_for('authentication.login'))
         return func(*args, **kwargs)
     return wrapper
@@ -19,6 +21,7 @@ def anonymous_required(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         if g.user:
+            flash('You have been already logged in')
             return redirect(url_for('view.index'))
         return func(*args, **kwargs)
     return wrapper
