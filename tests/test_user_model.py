bbecf8f94dc290021f4271ffd7999b90a4c217a4
Dima Afanasev <duma12let@gmail.com>
2021-04-22 16:30:02 +0300

    Add several User model tests

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,10 +1,13 @@
 import unittest
 from app import make_app
 from app import db
+from app import mail
 from app.config import TestConfig
 from app.authentication import User
 from app.authentication.exceptions import UserNotFoundByIndexError
+from itsdangerous.exc import SignatureExpired, BadSignature
 import os
+import time
 
 
 class UserModelTestCase(unittest.TestCase):
@@ -56,6 +59,53 @@ class UserModelTestCase(unittest.TestCase):
         user1.set_password(password)
         user2.set_password(password)
         self.assertNotEqual(user1.password_hash, user2.password_hash)
+
+    def test_send_mail(self):
+        user1 = User(email='user1@gmail.com', username='user1', password_hash='123')
+        user2 = User(email='user2@gmail.com', username='user2', password_hash='123')
+        db.session.add_all([user1, user2])
+        db.session.commit()
+        with mail.record_messages() as records:
+            user1.send_email(subject='Testing_subject1', text='testing_text1')
+            user2.send_email(subject='Testing_subject2', text='testing_text2')
+            self.assertEqual(len(records), 2)
+            self.assertEqual(records[0].subject, 'Testing_subject1')
+            self.assertEqual(records[0].body, 'testing_text1')
+            self.assertEqual(records[1].subject, 'Testing_subject2')
+            self.assertEqual(records[1].body, 'testing_text2')
+
+    def test_reset_password_token(self):
+        user1 = User(email='user1@gmail.com', username='user1', password_hash='123')
+        user2 = User(email='user2@gmail.com', username='user2', password_hash='123')
+        db.session.add_all([user1, user2])
+        db.session.commit()
+        token1 = user1.get_reset_password_token()
+        token2 = user2.get_reset_password_token()
+        self.assertEqual(user1, User.get_user_by_reset_password_token(token1))
+        self.assertEqual(user2, User.get_user_by_reset_password_token(token2))
+
+    def test_expired_password_token(self):
+        user = User(email='user@gmail.com', username='user', password_hash='123')
+        db.session.add(user)
+        db.session.commit()
+        token = user.get_reset_password_token(1)
+        time.sleep(2)
+        with self.assertRaises(SignatureExpired):
+            User.get_user_by_reset_password_token(token)
+
+    def test_bad_signature_password_token(self):
+        user = User(email='user@gmail.com', username='user', password_hash='123')
+        db.session.add(user)
+        db.session.commit()
+        token = user.get_reset_password_token()
+        token = token[:10]
+        with self.assertRaises(BadSignature):
+            User.get_user_by_reset_password_token(token)
+
+
+
+
+
 
 
 
