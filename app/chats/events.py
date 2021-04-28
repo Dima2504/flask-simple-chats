6b0aa9a21cc7b841abfbdb6d6739c980a04fbf7c
Dima Afanasev <duma12let@gmail.com>
2021-04-28 12:49:10 +0300

    Add socket events which allow to communicate with another user
    User cat be put through to another registered person and talk.
    Currently, events don't perform operations in order to save messages. It
    will be developed soon.

--- app/chats/events.py
+++ app/chats/events.py
@@ -1,7 +1,42 @@
 from .. import socket_io
+from flask import session
+from flask_socketio import emit, join_room, leave_room
+from flask_socketio import Namespace
 
 
-@socket_io.on('connect', namespace='/chats/going')
-def c():
-    print('connected')
+class ChatRoomNamespace(Namespace):
+    def on_connect(self):
+        pass
 
+    def on_disconnect(self):
+        pass
+
+    def on_enter_room(self):
+        """Is sent by client when it connects to the sever. Gets room name and user's name from the session and joins
+        the room. Sends a message on status handler only for debug."""
+        room_name = session.get('room_name')
+        user_name = session.get('user_name')
+        join_room(room_name)
+        emit('status', {'message': f'{user_name} entered the room'}, room=room_name)
+
+    def on_put_data(self, data: dict):
+        """
+        Receives message and time of writing and redirects it into print message handler on client.
+        Broadcasts to all people in room (to the current user and to a companion).
+        :param data: json from client which contains message and timestamp
+        :type data: dict
+        """
+        room_name = session.get('room_name')
+        user_name = session.get('user_name')
+        emit('print_message', {'message': data['message'], 'user_name': user_name}, room=room_name)
+
+    def on_leave_room(self):
+        """Sent by client when it leaves the room. Remove variables from user session, leaves room and sends
+        status message for debug."""
+        room_name = session.pop('room_name')
+        user_name = session.pop('user_name')
+        leave_room(room_name)
+        emit('status', {'message': f'{user_name} left the room'}, room=room_name)
+
+
+socket_io.on_namespace(ChatRoomNamespace('/chats/going'))
---
+++ app/static/js/socket_chat.js
@@ -0,0 +1,35 @@
+let socket;
+document.addEventListener('DOMContentLoaded', () => {
+    socket = io(window.location.href); // /chats/going
+    socket.on('connect', function () {
+        console.log('connected');
+        socket.emit('enter_room');
+    });
+
+    socket.on('status', function (data) {
+        console.log(data.message);
+    });
+
+    socket.on('print_message', function (data) {
+        console.log(data['user_name'] + ': ' + data['message']);
+        let area = document.querySelector('#chat');
+        area.value = area.value + data['user_name'] + ': ' + data['message'] + '\n';
+    });
+
+    let send_message_form = document.querySelector('#send_message_form');
+    send_message_form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let message = new FormData(send_message_form).get('message').trim();
+        if (message.length !== 0) {
+            socket.emit('put_data', {'message': message, 'timestamp_seconds': Date.now() / 1000});
+            send_message_form.querySelector('#message').value = '';
+        }
+    });
+});
+
+function leave_room() {
+    socket.emit('leave_room', () => {
+        socket.disconnect();
+        window.location.href = '/';
+    });
+}
