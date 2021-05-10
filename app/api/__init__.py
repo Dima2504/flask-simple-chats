07fef3181dbf68d7a1f42a4e1900ba2814b31dba
Dima Afanasev <duma12let@gmail.com>
2021-05-10 15:39:33 +0300

    Add messages resource to api: get, post, delete and put methods
    Now api user can send, see and alter messages from the certain chat. Add
    some abort_if... into api.utils

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -8,10 +8,15 @@ api = Api(api_bp)
 from .auth import Register, Token
 from .resources.chats import ChatsList, ChatSingle
 from .resources.users import UsersList, UserSingle
+from .resources.messages import ChatMessagesList, ChatMessageSingle
 
 api.add_resource(Register, '/register', strict_slashes=False)
 api.add_resource(Token, '/token', strict_slashes=False)
+
 api.add_resource(ChatsList, '/chats', strict_slashes=False)
 api.add_resource(ChatSingle, '/chats/<int:chat_id>', strict_slashes=False)
+api.add_resource(ChatMessagesList, '/chats/<int:chat_id>/messages', strict_slashes=False)
+api.add_resource(ChatMessageSingle, '/chats/<int:chat_id>/messages/<int:message_id>', strict_slashes=False)
+
 api.add_resource(UsersList, '/users', strict_slashes=False)
 api.add_resource(UserSingle, '/users/<int:user_id>', strict_slashes=False)
--- app/api/utils.py
+++ app/api/utils.py
@@ -1,6 +1,8 @@
 from sqlalchemy.engine.row import Row
 from app.authentication.models import User, chats
 from app.authentication.exceptions import UserNotFoundByIndexError
+from app.chats.models import Message
+from app.chats.exceptions import MessageNotFoundByIndexError
 from flask_restful import abort
 from app import db
 
@@ -19,9 +21,29 @@ def abort_if_not_a_participant(user_id: int, chat: Row):
         abort(403, message=f'You are not a participant of this chat {chat.chat_id}')
 
 
+def abort_if_not_from_a_chat(chat_id: int, message: Message):
+    """Makes an abort if the message  is not from the chat with the given id"""
+    if message.chat_id != chat_id:
+        abort(404, message=f'Message {message.message_id} is not from the chat {chat_id}')
+
+
+def abort_if_not_own(user_id: int, message: Message):
+    """Makes an abort if the given message was sent not by a user with the given id"""
+    if message.sender_id != user_id:
+        abort(400, message=f'Message {message.message_id} does not belong to you')
+
+
 def return_user_or_abort(user_id: int) -> 'User':
     """Returns user model instance if it exists, else - makes abort"""
     try:
         return User.get_user_by_id(user_id)
     except UserNotFoundByIndexError:
         abort(404, message=f'User {user_id} does not exist')
+
+
+def return_message_or_abort(message_id: int) -> 'Message':
+    """Returns message instance by id if it exists, if not - raises abort"""
+    try:
+        return Message.get_message_by_id(message_id)
+    except MessageNotFoundByIndexError:
+        abort(404, message=f'Message {message_id} does not exist')
