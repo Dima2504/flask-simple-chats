cd9452fc5fda0d6572935e7d3fe0a59fa3ef28c0
Dima Afanasev <duma12let@gmail.com>
2021-05-13 17:54:05 +0300

    Make "flask tests" command return 1 code, if there are mistages in tests

--- app/commands.py
+++ app/commands.py
@@ -2,6 +2,7 @@
 clearly to split application logic and understand it.
 """
 import unittest
+import sys
 from flask import Blueprint
 
 cli_commands = Blueprint('cli_commands', __name__, cli_group=None)
@@ -11,4 +12,9 @@ cli_commands = Blueprint('cli_commands', __name__, cli_group=None)
 def tests_command():
     """Run all the tests from here"""
     tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+    runner = unittest.TextTestRunner(verbosity=2)
+    result = runner.run(tests)
+    if result.wasSuccessful():
+        sys.exit(0)
+    else:
+        sys.exit(1)
